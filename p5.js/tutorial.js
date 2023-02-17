cbb210210a2b3fed27ad74f90667c67c8693f84c
unobatbayar <usukhbayar.batbayar@music.yamaha.com>
2023-02-17 14:02:50 +0900

    learning p5.js!

---
+++ p5.js/tutorial.js
@@ -0,0 +1,16 @@
+﻿function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  
+  let c = color(255, 0, 0);
+  let d = color(255, 204, 0);
+  
+ if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(c);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
